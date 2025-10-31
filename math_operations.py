0515f9acee4f7dc3ecf421a916d9955302cc6e83
Akhtarnaaz <01fe24ica054@kletech.ac.in>
2025-10-31 13:16:51 +0530

    Program for add & subtract

---
+++ math_operations.py
@@ -0,0 +1,14 @@
+# Program to perform addition and subtraction of two numbers
+
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Performing addition and subtraction
+sum_result = num1 + num2
+diff_result = num1 - num2
+
+# Displaying the results
+print("\nResults:")
+print(f"Addition: {num1} + {num2} = {sum_result}")
+print(f"Subtraction: {num1} - {num2} = {diff_result}")
